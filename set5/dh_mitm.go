b5b01a96d6cc341bbe8ac3608f429afec23aa7e2
Dhananjay Balan <mail@dbalan.in>
2019-02-12 14:15:43 -0500

    Impliment regular communication

---
+++ set5/dh_mitm.go
@@ -0,0 +1,92 @@
+package set5
+
+import (
+	"github.com/dbalan/cryptopals/common"
+	"github.com/dbalan/cryptopals/set2"
+	"github.com/dbalan/cryptopals/sha"
+	"math/big"
+)
+
+type Recv interface {
+	Kex(pub *big.Int) *big.Int
+	Exchange(ct, iv []byte) (msg, niv []byte)
+}
+
+type PersonB struct {
+	p, g, pub, priv, theirPub, skey *big.Int
+}
+
+func newB() *PersonB {
+	p, g := primes()
+	pub, priv := keypair(p, g)
+	return &PersonB{p: p, g: g, pub: pub, priv: priv}
+}
+
+func (b *PersonB) Kex(pub *big.Int) *big.Int {
+	b.theirPub = pub
+	skey := sessionKey(pub, b.priv, b.p)
+	b.skey = skey
+	return b.pub
+}
+
+func (b *PersonB) Exchange(ct, iv []byte) ([]byte, []byte) {
+	// decrypt!
+	msg := decryptWithSK(b.skey, ct, iv)
+	nct, niv := encryptWithSK(b.skey, msg)
+	return nct, niv
+}
+
+func (b *PersonB) Skey() *big.Int {
+	return b.skey
+}
+
+func getKeySK(skey *big.Int) []byte {
+	keybytes := []byte(skey.Text(16))
+	key := []byte(common.EncodeHexString(sha.SHA(keybytes))[0:16])
+	return key
+}
+
+func encryptWithSK(skey *big.Int, msg []byte) ([]byte, []byte) {
+	// get an AES key
+	key := getKeySK(skey)
+
+	iv, err := common.RandBytes(16)
+	if err != nil {
+		panic(err)
+	}
+
+	ct, err := set2.EncAES128CBC(msg, iv, key)
+	if err != nil {
+		panic(err)
+	}
+
+	return ct, iv
+}
+
+func decryptWithSK(skey *big.Int, ct, iv []byte) []byte {
+	key := getKeySK(skey)
+	msg, err := set2.DecAES128CBC(ct, iv, key)
+	if err != nil {
+		panic("error: decrypt")
+	}
+	return msg
+}
+
+func normComm() {
+	// personA
+	recv := newB()
+	msg := []byte("hello world")
+
+	p, g := primes()
+	pub, priv := keypair(p, g)
+	pubtheir := recv.Kex(pub)
+	skey := sessionKey(pubtheir, priv, p)
+
+	ct, iv := encryptWithSK(skey, msg)
+	nct, niv := recv.Exchange(ct, iv)
+	rmsg := decryptWithSK(skey, nct, niv)
+
+	if !common.EqualBlocks(rmsg, msg) {
+		panic("msgs don't match")
+	}
+}
---
+++ set5/dh_mitm_test.go
@@ -0,0 +1,9 @@
+package set5
+
+import (
+	"testing"
+)
+
+func TestTestNormalComm(t *testing.T) {
+	normComm()
+}
