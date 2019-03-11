f00c44edc84095c9fbf1d6add7d1cbbecae79608
Dhananjay Balan <mail@dbalan.in>
2019-03-11 19:15:58 -0400

    Attack the parsing vuln. in cryptopals question!!

--- set6/rsa_sign.go
+++ set6/rsa_sign.go
@@ -1,93 +1,81 @@
 package set6
 
 import (
-	"fmt"
 	"github.com/dbalan/cryptopals/sha"
 	"math/big"
 )
 
+var (
+	shaPfx = []int{0x30, 0x21, 0x30, 0x09, 0x06, 0x05, 0x2b, 0x0e, 0x03, 0x02,
+		0x1a, 0x05, 0x00, 0x04, 0x14}
+)
+
 func forgeSig(data []byte, N *big.Int) {
 	hs := sha.SHA(data)
-	shaPfx := []int{0x30, 0x21, 0x30, 0x09, 0x06, 0x05, 0x2b, 0x0e, 0x03, 0x02,
-		0x1a, 0x05, 0x00, 0x04, 0x14}
 
-	assemble := []byte{byte(0xff), byte(0x00)}
+	assemble := []byte{byte(0x00), byte(0x01), byte(0xff), byte(0x00)}
 	for _, b := range shaPfx {
 		assemble = append(assemble, byte(b))
 	}
+	actualsig := make([]byte, len(assemble))
+	copy(actualsig, assemble)
 
 	assemble = append(assemble, hs...)
 
-	// (00 + ff + 00 + assemble + garbage)** 1/3
-	fmt.Printf("%b\n", assemble)
+	garbage := []byte{byte(0x00)}
+
+	// pad with garbage to module size.
+	for i := 0; i < 1024-(len(assemble)*8); i++ {
+		garbage = append(garbage, byte(0xff))
+	}
+	assemble = append(assemble, garbage...)
+	//	println("len: ", len(assemble)*8)
+
+	// (0x00 + 0x01 0xff + 0x00 + assemble + garbage)** 1/3
 	destPfx := new(big.Int).SetBytes(assemble)
-	src := oddCubeRootPrefix(destPfx)
+	croot := findClosestCube(destPfx)
 
-	println(src.Text(16))
-}
+	// signature verification
+	// FIXME: move out
+	croot.Exp(croot, big.NewInt(3), nil)
 
-func cubed(src *big.Int) string {
-	s := new(big.Int).Set(src)
-	s.Exp(s, big.NewInt(3), nil)
-	return s.Text(2)
-}
-
-func cmpBit(dest, src string, i int) bool {
-	var s byte
-	// src is less than that bit, so its zero
-	if (len(src) - i - 1) < 0 {
-		s = byte('0')
-	} else {
-		s = byte(src[len(src)-i-1])
-	}
-	return byte(dest[len(dest)-1-i]) == s
-}
-
-func flip(src string, i int) string {
-	if len(src)-1-i < 0 {
-		ns := ""
-		padLen := i - (len(src) - 1)
-		for p := 0; p < padLen; p++ {
-			ns += "0"
-		}
-
-		ns += src
-		src = ns
+	sig := croot.Text(16)
+	asig := new(big.Int).SetBytes(actualsig)
+	expected := asig.Text(16)
+	if sig[0:len(expected)] == expected {
+		println("Signature verification passed.")
 	}
 
-	var nb byte
-	if []byte(src)[len(src)-1-i] == byte('1') {
-		nb = byte('0')
-	} else {
-		nb = byte('1')
-	}
-
-	s := []byte(src)
-	s[len(src)-1-i] = nb
-	return string(s)
 }
 
-func flipbigInt(s *big.Int, i int) {
-	dst := flip(s.Text(2), i)
-	s.SetString(dst, 2)
-}
+func findClosestCube(a *big.Int) *big.Int {
+	low := big.NewInt(0)
+	hi := new(big.Int).Set(a)
 
-func oddCubeRootPrefix(destPrefx *big.Int) *big.Int {
-	pfx := destPrefx.Text(2)
-	src := big.NewInt(1)
+	for low.Cmp(hi) == -1 {
+		mid := new(big.Int).Add(low, hi)
+		mid.Div(mid, big.NewInt(2))
 
-	for i := 0; i < len(pfx); i++ {
-		// cmp ith bit from last, flip in src
-		csrc := cubed(src)
+		m3 := new(big.Int).Exp(mid, big.NewInt(3), nil)
 
-		if !cmpBit(pfx, csrc, i) {
-			//println(pfx)
-			// println(csrc)
-			//println("will flip bit ", i)
-			//flip i t bit in src
-			flipbigInt(src, i)
+		if m3.Cmp(a) == -1 {
+			low = mid.Add(mid, big.NewInt(1))
+		} else {
+			hi.Set(mid)
 		}
 	}
-
-	return src
+	return low
 }
+
+/*
+def true_cbrt(n):
+    lo = 0
+    hi = n
+    while lo < hi:
+        mid = (lo+hi)//2
+        if mid**3 < n:
+            lo = mid+1
+        else:
+            hi = mid
+    return lo
+*/
