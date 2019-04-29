7161607d8be21980f3967788a84838cdfbf0e0a5
Dhananjay Balan <mail@dbalan.in>
2019-04-29 15:10:32 +0200

    Break an RSA Parity oracle

--- set6/rsa_parity_oracle.go
+++ set6/rsa_parity_oracle.go
@@ -12,7 +12,7 @@ const secret = "VGhhdCdzIHdoeSBJIGZvdW5kIHlvdSBkb24ndCBwbGF5" +
 	"IGFyb3VuZCB3aXRoIHRoZSBGdW5reSBDb2xkIE1lZGluYQ=="
 
 // takes a ciphertext, returns true of PT is an even number.
-func ParityOracle() (*big.Int, func(*big.Int) bool) {
+func ParityOracle() (*big.Int, *big.Int, *big.Int, func(*big.Int) bool) {
 	pub, priv, n, err := rsa.GenKeyPair(1024)
 	if err != nil {
 		panic(err)
@@ -23,21 +23,71 @@ func ParityOracle() (*big.Int, func(*big.Int) bool) {
 	actualCT := rsa.Encrypt(msg, pub, n)
 
 	oracle := func(ct *big.Int) bool {
-		msg, err := rsa.Decrypt(ct, priv, n)
-		if err != nil {
-			panic(err)
-		}
-
-		return isEven(msg)
+		pt := new(big.Int).Exp(ct, priv, n)
+		pt.Mod(pt, big.NewInt(2))
+		return pt.Cmp(big.NewInt(0)) == 0
 	}
 
-	return actualCT, oracle
+	return actualCT, pub, n, oracle
 }
 
-func isEven(msg []byte) bool {
-	hx := fmt.Sprintf("%x", msg)
-	m := new(big.Int)
-	m.SetString(hx, 16)
-	m.Mod(m, big.NewInt(2))
-	return m.Cmp(big.NewInt(0)) == 0
+func times(n *big.Int) int {
+	tmp := new(big.Int).Set(n)
+	zero := big.NewInt(0)
+	res := 0
+	for {
+		tmp.Rsh(tmp, 1)
+		if tmp.Cmp(zero) <= 0 {
+			break
+		}
+		res++
+	}
+	return res
+}
+
+func parityOracleAttack(e, n, ct *big.Int,
+	oracle func(*big.Int) bool) {
+
+	// set bounds
+	lower := big.NewInt(0)
+	upper := new(big.Int).Set(n)
+
+	// generate a multiplier
+	multi := big.NewInt(2)
+	multi.Exp(multi, e, n)
+
+	for i := 0; i < times(n); i++ {
+		// multiply ct
+		ct.Mul(ct, multi)
+		ct.Mod(ct, n)
+
+		if oracle(ct) {
+			// less than half of upper
+			upper.Add(upper, lower)
+			upper.Div(upper, big.NewInt(2))
+		} else {
+			// more than half of lower
+			lower.Add(upper, lower)
+			lower.Div(lower, big.NewInt(2))
+		}
+
+		// we probably found it
+		if upper.Cmp(lower) == 0 {
+			break
+		}
+
+		printMsg(upper)
+	}
+	printMsg(upper)
+}
+
+func printMsg(pt *big.Int) {
+	var msg []byte
+	_, err := fmt.Sscanf(pt.Text(16), "%x", &msg)
+	if err != nil {
+		// We're gonna have EOF Errors
+		fmt.Println("decoding errors")
+		return
+	}
+	fmt.Println("Decrypted:", string(msg))
 }
--- set6/rsa_parity_oracle_test.go
+++ set6/rsa_parity_oracle_test.go
@@ -1,10 +1,22 @@
 package set6
 
 import (
+	"github.com/stretchr/testify/assert"
+	"math/big"
 	"testing"
 )
 
 func TestOracle(t *testing.T) {
-	ct, oracle := ParityOracle()
+	ct, _, _, oracle := ParityOracle()
 	oracle(ct)
+}
+
+func TestTimes(t *testing.T) {
+	res := times(big.NewInt(16))
+	assert.Equal(t, 4, res)
+}
+
+func TestBreakOracle(t *testing.T) {
+	ct, e, n, oracle := ParityOracle()
+	parityOracleAttack(e, n, ct, oracle)
 }
